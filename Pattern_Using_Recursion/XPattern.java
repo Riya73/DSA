8fb176b920bd40871105d0180a17816330e1292a
Riya73 <72311878+Riya73@users.noreply.github.com>
2023-06-11 12:25:33 +0530

    in

---
+++ Pattern_Using_Recursion/XPattern.java
@@ -0,0 +1,31 @@
+package Patterns_Using_Recursion;
+
+public class XPattern {
+    static void printX(int rows, int starsOrSpaces) {
+        if (rows == 0) {
+            return;
+        }
+
+        printX(rows - 1, starsOrSpaces);
+        printStarsOrSpaces(rows, starsOrSpaces);
+        System.out.println();
+    }
+
+    static void printStarsOrSpaces(int rows, int starsOrSpaces) {
+        if (starsOrSpaces == 0) {
+            return;
+        }
+
+        printStarsOrSpaces(rows, starsOrSpaces - 1);
+
+        if (rows + starsOrSpaces == 6 || rows == starsOrSpaces) {
+            System.out.print("* ");
+        } else {
+            System.out.print("  ");
+        }
+    }
+
+    public static void main(String[] args) {
+        printX(5, 5);
+    }
+}
