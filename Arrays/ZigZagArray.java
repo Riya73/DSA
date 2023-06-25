3bec9a304f5c458c1a45932e846b85ebb5e578c2
Riya73 <72311878+Riya73@users.noreply.github.com>
2023-06-25 23:55:11 +0530

    Create ZigZagArray

---
+++ Arrays/ZigZagArray.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+public class ZigZagArray {
+    private static void convertToZigZag(int[] arr, int n) {
+        if (n <= 1) {
+            return;
+        }
+
+        // Swap the elements at odd index positions if they are greater
+        for (int i = 0; i < n - 1; i += 2) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+
+        // Swap the elements at even index positions if they are smaller
+        for (int i = 1; i < n - 1; i += 2) {
+            if (arr[i] < arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+
+        // Recursively call the function for the remaining subarray
+        convertToZigZag(arr, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int arr [] ={4,3,7,8,6,2,1};
+        int n = arr.length;
+        convertToZigZag(arr, n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+            }
+    }
+}
+
